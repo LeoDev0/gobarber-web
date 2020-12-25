3f498f61c21c2622572ebc83e8736a930b3265de
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-12-25 19:03:15 -0300

    add styles to login page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="gobarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="teste">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
